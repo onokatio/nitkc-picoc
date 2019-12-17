9fa53f1e75f73cbd1fa6651bae2a07d426c87032
onokatio <onokatio@gmail.com>
2019-12-17 15:26:43 +0900

    run all test

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
-ls test*.c | while read line;do ../test_parser < $line > out/$(echo $line | sed -e 's/\.c//').out ;done
-ls test*.c | while read line;do ../test_parser_answer < $line > sample_out/$(echo $line | sed -e 's/\.c//').out ;done
+ls *.c | while read line;do ../test_parser < $line > out/$(echo $line | sed -e 's/\.c//').out ;done
+ls *.c | while read line;do ../test_parser_answer < $line > sample_out/$(echo $line | sed -e 's/\.c//').out ;done
 ls out | while read line;do echo $line;diff out/$line sample_out/$line;done
